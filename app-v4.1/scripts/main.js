76894bee0065b59d0dab5e7d9b23fd42a407f529
ever <cui_xl@163.com>
2017-04-23 23:27:09 +0800

    slide banner and slide

--- app-v4.1/scripts/main.js
+++ app-v4.1/scripts/main.js
@@ -1,26 +1,10 @@
 
-$(".ge-lazy-pic img").lazyload({effect : "fadeIn"});
+$('.ge-lazy-pic img').lazyload({effect : 'fadeIn'});
 
-// $("#sticker").sticky({topSpacing:0});
-
-
-var mySwiper = new Swiper('.ge-mart-slide-tab', {
-	slidesPerView:'auto',
-	spaceBetween:20,
-	slidesOffsetBefore:20,
-	slidesOffsetAfter:20,
-
-	wrapperClass:'slide-wrapper',
-	slideClass:'slide-item',
-	slideToClickedSlide:true,
-
-	onTap: function(swiper){
-      for (var i = 0; i < swiper.slides.length ; i++) {
-	        swiper.slides[i].className ='slide-item'; 
-	    }
-	    swiper.clickedSlide.className +=" "+ 'curr';
-    },
-    onInit:function (swiper) {
-    	swiper.slides[0].className +=" "+ 'curr';
-    }
+$('.ge-mart-banner').slick({
+	arrows:false,
+	lazyLoad:'ondemand',
+	// autoplay:5000,
+	dots:true,
 })
+
