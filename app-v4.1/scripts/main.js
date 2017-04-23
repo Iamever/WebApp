bdafd55a7454ecacbbc3c56772c2ec4fc83f6328
ever <cui_xl@163.com>
2017-04-23 15:12:45 +0800

    swiper 横向导航

--- app-v4.1/scripts/main.js
+++ app-v4.1/scripts/main.js
@@ -4,46 +4,23 @@ $(".ge-lazy-pic img").lazyload({effect : "fadeIn"});
 // $("#sticker").sticky({topSpacing:0});
 
 
+var mySwiper = new Swiper('.ge-mart-slide-tab', {
+	slidesPerView:'auto',
+	spaceBetween:20,
+	slidesOffsetBefore:20,
+	slidesOffsetAfter:20,
 
-var banner = new Swiper('.banner-swiper-container', {
-	autoplay: 5000,
-	wrapperClass :'banner-swiper-wrap',
-	slideClass :'banner-swiper-item',
-	pagination : '.banner-swiper-dots',
-	lazyLoading : true,
-	paginationClickable :true,
-	loop:true,
-})
+	wrapperClass:'slide-wrapper',
+	slideClass:'slide-item',
+	slideToClickedSlide:true,
 
-var s_nav = new Swiper('.nav-swiper-container',{
-	wrapperClass :'nav-swiper-wrap',
-	slideClass :'nav-swiper-item',
-
-	// freeMode : true,
-	slidesPerView : 'auto',
-	// spaceBetween :25,
-	// slidesOffsetAfter:20,
-	// slidesOffsetBefore:20,
-
-	// watchSlidesProgress :true,
-	// watchSlidesVisibility : true,
-
-	// slideToClickedSlide:true,
-
-	// loop:true,
-	// loopedSlides:7,
-
-	// centeredSlides : true,
-	onClick: function(swiper){
-    	swiper.slideTo(swiper.clickedIndex);
-
-    	for (var i = 0; i < swiper.slides.length ; i++) {
-	        swiper.slides[i].className ='nav-swiper-item'; 
+	onTap: function(swiper){
+      for (var i = 0; i < swiper.slides.length ; i++) {
+	        swiper.slides[i].className ='slide-item'; 
 	    }
-	    	swiper.clickedSlide.className +=" "+ 'nav-swiper-active'; 
+	    swiper.clickedSlide.className +=" "+ 'curr';
     },
     onInit:function (swiper) {
-    	swiper.slideTo(0);
-    	swiper.slides[0].className +=" "+ 'nav-swiper-active'; 
+    	swiper.slides[0].className +=" "+ 'curr';
     }
 })
