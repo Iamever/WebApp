9d2405bbc715ab38d339d07fa3750208f7d87d13
cuixiaoli <cuixiaoli>
2017-04-24 18:37:11 +0800

    .

--- app-v4.1/scripts/css-base.js
+++ app-v4.1/scripts/css-base.js
@@ -3,7 +3,13 @@
     document.addEventListener('DOMContentLoaded', function () {
         var html = document.documentElement;
         var windowWidth = html.clientWidth;
-        html.style.fontSize = windowWidth / 7.5 + 'px';
+        
+        if(windowWidth >640){
+        	html.style.fontSize = 50 + 'px'; // 按照iphone6的尺寸显示在PC端
+        }else{
+        	html.style.fontSize = windowWidth / 7.5 + 'px';
+        }
+
         // 等价于html.style.fontSize = windowWidth / 750 * 100 + 'px';
     }, false);
 })();
--- app-v4.1/scripts/main.js
+++ app-v4.1/scripts/main.js
@@ -1,3 +1,32 @@
+/**
+ * 使用插件：
+ * Jquery
+ * jieyou_lazyload
+ * layer
+ */
+$('.ge-radio-green,.ge-check-main').click(function() {
+	$(this).toggleClass('checked');
+});
+$('.go2pay').click(function() {
+	  //页面层
+	  layer.open({
+	    type: 1
+	    ,content: '<input class="password" type="password" placeholder="请输入内购密码"/>'
+	    ,anim: 'up'
+	    ,style: 'width:80%;border-radius:0.1rem;'
+	    ,btn: ['确定','取消']
+	    ,className:'ge-layer-1'
+	    ,yes: function(index){
+		  layer.open({
+		    content: '密码错误'
+		    ,skin: 'msg'
+		    ,time: 2 
+		  });
+		}  
+	  });
+});
+
+
 
 $('.ge-lazy-pic img').lazyload({effect : 'fadeIn'});
 
@@ -8,3 +37,24 @@ $('.ge-mart-banner').slick({
 	dots:true,
 })
 
+
+var mySwiper = new Swiper('.ge-mart-slide-tab', {
+	wrapperClass:'slide-wrap',
+	slideClass :'slide-item',
+
+	slidesPerView :'auto',
+	spaceBetween :20,
+
+	freeMode :true,
+
+	onInit:function (swiper) {
+		// swiper.slides[0].className += ' ' + 'slide-curr' ;
+	},
+
+	onTap:function (swiper) {
+		for(var i = 0;i < swiper.slides.length ; i++){
+			// swiper.slides[i].className = 'slide-item';
+		}
+		// swiper.slides[swiper.clickedIndex].className += ' ' + 'slide-curr' ;
+	}
+})
