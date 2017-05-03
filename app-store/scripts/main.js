bc61fff858ef6d84f09899e094d4ae454cf1c5cc
cuixiaoli <cuixiaoli>
2017-05-03 18:55:21 +0800

    编写dialog插件

--- app-store/scripts/main.js
+++ app-store/scripts/main.js
@@ -6,4 +6,30 @@
 $('.ge-comm-tab .tab-item').on('click', function() {
 	
 	$(this).addClass('curr').siblings().removeClass('curr');
+	var tab_index = $(this).index();
+	$('.ge-comm-tabcon .tabcon-item').eq(tab_index).addClass('curr').siblings().removeClass('curr');
 });
+
+
+$('#openLayer').on('click', function() {
+	var dia = new geDialog({
+		message:'<form class="distribution_input"><div class="ge-flex"><label for="name">配送人: </label><input type="text" placeholder="配送人姓名"></div><div class="ge-flex"><label for="name">手机号: </label><input type="tel" placeholder="配送人手机号"></div><div class="ge-flex"><label for="name" class="text-gray">备注:</label><input type="text" ></div></form>',
+		close_btn:true,
+		opacity:0.3,
+		buttons:[
+        	{
+	          text:'开始配送',
+	          type:'ok',
+	          callback:function () {
+	          	new geDialog({
+	          		message:'已配送',
+	          		delay:2000,
+	          		coverClose:true,
+	          	})
+	            dia.close();
+	          }
+	        }
+        ],
+	});
+});
+
