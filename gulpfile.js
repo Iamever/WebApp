7d31564a376d9b51fbb4527c0de45311fff825ae
ever <cui_xl@163.com>
2017-05-07 14:21:30 +0800

    抽取单独css

--- gulpfile.js
+++ gulpfile.js
@@ -105,7 +105,7 @@ gulp.task('serve', () => {
       port: 9000,
       server: {
         baseDir: ['.tmp', app_path_name],
-        index: "ge-home.html",
+        index: "ge-pay.html",
         routes: {
           '/bower_components': 'bower_components'
         }
